f7911ad2c7070cdabc0dee3c28f3eab3e7f3d5dd
Kaboggoza Rajjab <rajjabkaboggoza@gmail.com>
2022-10-03 07:03:42 -0700

    Commited by RAJJAB

---
+++ std2/view/footer.php
@@ -0,0 +1,7 @@
+	<footer class="main-footer">
+        
+        <strong>Copyright &copy; 2022 secondary school management system .</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
