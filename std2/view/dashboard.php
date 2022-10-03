b9f2740708cdfbc64a1a7225b4d7a496013e7f8f
Kaboggoza Rajjab <rajjabkaboggoza@gmail.com>
2022-10-03 07:23:07 -0700

    Update dashboard.php
    Have deleted the un necessary space on the dashbord.php

--- std2/view/dashboard.php
+++ std2/view/dashboard.php
@@ -145,9 +145,7 @@ margin:0;
 <div class="content-wrapper">
 	<!-- Content Header (Page header) -->
     <section class="content-header">
-    	<h1>
-        	Dashboard
-        	<small>Preview</small>
+    	<h1>Dashboard<small>Preview</small>
         </h1>
         <ol class="breadcrumb">
         	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
